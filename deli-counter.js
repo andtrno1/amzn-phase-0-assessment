e1bcf43e72d880a50b3e4cdac8219c59cb67964b
andtrno1 <andtrno1@gmail.ccom>
2022-04-15 21:18:29 -0400

    Completed Assignment

--- deli-counter.js
+++ deli-counter.js
@@ -22,3 +22,45 @@
 // nowServing(katzDeli) //=> "Currently serving Grace."
 
 // line(katzDeli) //=> "The line is currently: 1. Kent 2. Matz"
+
+const katzDeli =[];
+
+function takeANumber(katzDeli, name){
+    katzDeli.push(name);
+    console.log(`Welcome, ${name}. You are number ${katzDeli.length} in line `);
+}
+
+function line(katzDeli){
+    if(katzDeli.length === 0){
+        console.log("Nobody in the line");
+    }
+    else{
+        for(i=0; i<katzDeli.length; i++){
+            console.log(`The line is currently: ${i+1}. ${katzDeli.length}`)
+        }
+    }
+}
+
+function nowServing(katzDeli){
+    if(katzDeli.length === 0){
+        console.log("There is nobody waiting to be served!")
+    }
+    else if(katzDeli > 0){
+        var Serving = katzDeli[0];
+        katzDeli.shift();
+        console.log(`Currently Serving ${Serving}`);
+    }
+}
+
+
+takeANumber(katzDeli, "Thien");
+nowServing(katzDeli);
+line(katzDeli);
+
+takeANumber(katzDeli, "Bob");
+nowServing(katzDeli);
+line(katzDeli);
+
+takeANumber(katzDeli, "Mike");
+nowServing(katzDeli);
+line(katzDeli);
